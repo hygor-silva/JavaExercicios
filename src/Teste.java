0f56fa55e111788ae734b34cc57ffb893c636272
Hygor Silva <77420686+hygor-silva@users.noreply.github.com>
2021-09-02 10:31:23 -0300

    Exercício 1
    Formas geométricas

---
+++ src/Teste.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Escolha o que quer desenhar! ");
+		System.out.println("1 - Triângulo, 2 - Quadrado ou 3 - Círculo");
+		Scanner entrada = new Scanner(System.in);
+		
+		int num = entrada.nextInt();
+		
+		
+		
+		switch(num){
+		
+		case 1: 
+			System.out.println("Você escolheu desenha um Triângulo!");
+			System.out.println("Digite o valor da base do Triângulo!");
+			int baseT = entrada.nextInt();
+			int y =baseT;
+			int z = baseT;
+				for(y = 1; y<baseT;y++) {
+					for(z = 1; z<y; z++) {
+						System.out.print("*  ");
+					}System.out.println("");
+				}
+				for(z=baseT; z>1;z--) {
+					for(y = 1; y<z; y++) {
+						System.out.print("*  ");
+					}System.out.println("");
+			}
+			
+			break;
+			
+		case 2: 
+			System.out.println("Você escolheu desenha um Quadrado!");
+			System.out.println("Digite o valor da base do Quadrado!");
+			int baseQ = entrada.nextInt();
+			int i =baseQ;
+			int x = baseQ;
+				for(x = 0; x<baseQ; x++) {
+					for(i = 0; i<baseQ; i++) {
+						System.out.print("* ");
+					}
+					System.out.println("");
+				}
+			
+			break;
+			
+		case 3: 
+			System.out.println("Você escolheu desenha um Círculo!");
+			System.out.println("Digite o valor do raio do Círculo!");
+			int baseC = entrada.nextInt();
+			
+			break;
+		}
+
+	}
+
+}
---
+++ src/testeApp.java
@@ -0,0 +1,27 @@
+
+public abstract class testeApp {
+
+	public static void main(String[] args) {
+		
+		int i = 0;
+		int x = 0;
+		int z = 0;
+		
+		
+		
+			for(x = 8; x>1;x--) {
+				for(i = 1; i<x; i++) {
+					System.out.print("*");
+				}System.out.println("");
+				
+			}for(z=8; z>1;z--) {
+					for(i = 1; i<z; i++) {
+					System.out.print("*");
+		}System.out.println("");
+			}
+
+			
+			
+		}
+
+}
