024f44230660d1a92b86d4c1cbff4a9d11cdfd33
Hygor Silva <77420686+hygor-silva@users.noreply.github.com>
2021-09-02 11:14:44 -0300

    update triângulo
    Primeira parte do triângulo

--- src/testeApp.java
+++ src/testeApp.java
@@ -6,20 +6,30 @@ public abstract class testeApp {
 		int i = 0;
 		int x = 0;
 		int z = 0;
+		int y = 0;
 		
 		
 		
 			for(x = 8; x>1;x--) {
 				for(i = 1; i<x; i++) {
+					System.out.print(" ");
+					
+				}System.out.print("*");
+				for(y = 8; y>x; y--) {
 					System.out.print("*");
-				}System.out.println("");
+					
+				}
 				
-			}for(z=8; z>1;z--) {
-					for(i = 1; i<z; i++) {
-					System.out.print("*");
-		}System.out.println("");
+				System.out.println("");
+				
 			}
-
+			/*
+			for(z=8; z>1;z--) {
+				for(i = 1; i<z; i++) {
+				System.out.print("*");
+	}System.out.println("");
+		}
+*/
 			
 			
 		}
